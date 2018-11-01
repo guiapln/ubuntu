eb4baebadeacfd762a9ef60879e37cc45b79f2aa
guiapln <44686095+guiapln@users.noreply.github.com>
2018-11-01 20:09:45 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Meu script
+
+#Listando os diretórios do Linux
+ls -lha /
+read
+clear
+
+#Atualizando o Apt ou APpt-Get
+apt update 
+echo "Listas atualizadas!!!"
+read
+cls
+
+#Desligando o servidor
+shutdown -h now
